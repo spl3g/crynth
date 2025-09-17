a5d309eec45728c533ef5be06a85abc7062bebb9
spl3g <speefer6@yandex.ru>
2025-09-17 17:20:57 +0300

    Add raylib to the dependencies

--- flake.nix
+++ flake.nix
@@ -3,41 +3,39 @@
 
   inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1";
 
-  outputs =
-    inputs:
-    let
-      supportedSystems = [
-        "x86_64-linux" # 64-bit Intel/AMD Linux
-        "aarch64-linux" # 64-bit ARM Linux
-        "x86_64-darwin" # 64-bit Intel macOS
-        "aarch64-darwin" # 64-bit ARM macOS
-      ];
+  outputs = inputs: let
+    supportedSystems = [
+      "x86_64-linux" # 64-bit Intel/AMD Linux
+      "aarch64-linux" # 64-bit ARM Linux
+      "x86_64-darwin" # 64-bit Intel macOS
+      "aarch64-darwin" # 64-bit ARM macOS
+    ];
 
-      forEachSupportedSystem =
-        f:
-        inputs.nixpkgs.lib.genAttrs supportedSystems (
-          system:
+    forEachSupportedSystem = f:
+      inputs.nixpkgs.lib.genAttrs supportedSystems (
+        system:
           f {
-            pkgs = import inputs.nixpkgs { inherit system; };
+            pkgs = import inputs.nixpkgs {inherit system;};
           }
-        );
-    in
-    {
-      devShells = forEachSupportedSystem (
-        { pkgs }:
-        {
-          default = pkgs.mkShell {
-            packages = with pkgs; [
-              gcc
-              stdenv.cc
-              gdb
-              
-              man-pages
-              
-              alsa-lib
-            ];
-          };
-        }
       );
-    };
+  in {
+    devShells = forEachSupportedSystem (
+      {pkgs}: {
+        default = pkgs.mkShell {
+          packages = with pkgs; [
+            gcc
+            stdenv.cc
+            gdb
+
+            man-pages
+            man-pages-posix
+
+            alsa-lib
+
+            raylib
+          ];
+        };
+      }
+    );
+  };
 }
